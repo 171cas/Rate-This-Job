b1156a47e744fe96df031971a4e7bbb6056fcc2b
Cesar Solano <171cas@gmail.com>
2022-02-11 17:36:17 -0600

    about links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
@@ -23,41 +23,48 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className="page-container">
-      <h1>Rate This Job!</h1>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route exact path="/createBusiness">
-            <CreateBusinessForm />
-          </Route>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-          <Route exact path={["/business", "/business/:businessId"]}>
-            <BusinessBrowser />
-          </Route>
-          <Route exact path={["/edit/business/:businessId"]}>
-            <EditBusinessForm />
-          </Route>
-          <Route exact path="/review/business/:businessId">
-            <ReviewBrowser />
-          </Route>
-          <Route exact path={["/review/:reviewId"]}>
-            <ReviewDetail />
-          </Route>
-          <Route exact path={["/review"]}>
-            <ReviewAllBrowser />
-          </Route>
-        </Switch>
-      )}
-    </div>
+    <>
+      <div className="myheader">
+        <h5>Developed by:</h5> <h4>Cesar Solano</h4>
+        <a href="https://www.linkedin.com/in/cesar-solano-320211230/" target="_blank">linkedIn </a>
+        <a href="https://github.com/171cas" target="_blank">GitHub </a>
+      </div>
+      <div className="page-container">
+        <h1>Rate This Job!</h1>
+        <Navigation isLoaded={isLoaded} />
+        {isLoaded && (
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route exact path="/createBusiness">
+              <CreateBusinessForm />
+            </Route>
+            <Route path="/login">
+              <LoginFormPage />
+            </Route>
+            <Route path="/signup">
+              <SignupFormPage />
+            </Route>
+            <Route exact path={["/business", "/business/:businessId"]}>
+              <BusinessBrowser />
+            </Route>
+            <Route exact path={["/edit/business/:businessId"]}>
+              <EditBusinessForm />
+            </Route>
+            <Route exact path="/review/business/:businessId">
+              <ReviewBrowser />
+            </Route>
+            <Route exact path={["/review/:reviewId"]}>
+              <ReviewDetail />
+            </Route>
+            <Route exact path={["/review"]}>
+              <ReviewAllBrowser />
+            </Route>
+          </Switch>
+        )}
+      </div>
+    </>
   );
 }
 
